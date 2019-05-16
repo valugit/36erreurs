39d6d8b7aba8e1488ebb16aba71bc83647fc81ce
valugit <gmasselegoanvic@gmail.com>
2019-05-16 16:11:18 +0200

    ok

--- src/views/winners.php
+++ src/views/winners.php
@@ -7,7 +7,7 @@
     <div class="col-sm-3" style="margin-bottom:20px">
       <h4 class="cut"><?php echo $categories[$winner['category_id']]['title']?></h4>
           <div style="position:relative">
-              <img src="/nominee/image/<?php echo $winner['id']?>" class="img-responsive">
+              <img src="<?php echo $winner['pic_url']?>" class="img-responsive">
             </div>
             <div  class="cut nomine">
               <?php echo $winner['name']?>
